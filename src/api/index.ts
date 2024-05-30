a3db96ef3d1c573956aa942e9721bf10d2b736c5
Ahmed Abdulkadir <ahmedaaa_123@hotmail.com>
2024-05-30 19:23:20 +0100

    Opitmised service

--- src/api/index.ts
+++ src/api/index.ts
@@ -26,22 +26,29 @@ app.post('/translate', async (req, res) => {
     if (!targetLanguage) throw new Error("target language not found")
     if (!(words?.length > 0)) throw new Error("words to translate not found")
 
-    let safeWordsSet = new Set<string>()
+    const CONSOLE_TIME_LABEL = "Time taken for service to complete translation"
+    console.time(CONSOLE_TIME_LABEL)
+    // Utilise <Set> to avoid duplicate words
+    let checkedWordsSet = new Set<string>()
+    let safeWords: string[] = []
     for (let i = 0; i < words?.length; i++) {
       const word = words[i];
 
       // For the current word being iterated, ensure:
-      //   1. All character are valid (using regular expressions)
-      //   2. Word is correctly spelt (using "SpellChecker" package)
-      //   3. Word is in english (using "is-word" package)
+      //   1. Word has not already been checked in a previous iteration (using <Set>)
+      //   2. All character are valid (using regular expressions)
+      //   3. Word is correctly spelt (using "SpellChecker" package)
+      //   4. Word is in english (using "is-word" package)
       // Discard any words that do not meet these requirements
-      const isSanitised: boolean = allCharsValid(word) && !SpellChecker.isMisspelled(word) && englishWords.check(word)
+      const isSanitised: boolean = !checkedWordsSet.has(word) && allCharsValid(word) && !SpellChecker.isMisspelled(word) && englishWords.check(word)
 
-      // Utilise <Set> to avoid duplications
-      isSanitised && safeWordsSet.add(word)
+      checkedWordsSet.add(word)
+      isSanitised && safeWords.push(word)
     }
 
-    const processedWords = await Promise.all([...safeWordsSet].map(word => requestTranslation(word, targetLanguage)))
+    const processedWords = await Promise.all(safeWords.map(word => requestTranslation(word, targetLanguage)))
+    console.timeEnd(CONSOLE_TIME_LABEL)
+
     res.send({ words: processedWords, targetLanguage });
   } catch (error) {
     console.log('error', error)
